6ddc6f7d1b8260ade390fba765ea4447afe2c829
Lakshya Agarwal <37949887+agarwallakshya37@users.noreply.github.com>
2020-11-30 19:50:15 +0530

    Added comments in the smart contract

--- contracts/medRec.sol
+++ contracts/medRec.sol
@@ -5,14 +5,14 @@ contract medRec{
     
     // structure to store the patient information
     struct Patient{
-	    address doctorAddress;
-        address patientAddress;
+	address doctorAddress;
+       	address patientAddress;
         string doctorName;
         string patientName;
         string gender;
-	    uint age;
-	    string dateOfVisit;
-	    string purposeOfVisit;
+        uint age;
+	string dateOfVisit;
+	string purposeOfVisit;
         string uniqueKey;
     }
     
@@ -21,34 +21,37 @@ contract medRec{
     mapping(address=>uint)public noOfRecords; // mapping of all the records for individual patient
     mapping(address=>mapping(uint=>Patient))public patientRecord; //mapping for the all the patients record
     mapping(string=>Patient)public records;
-    // mapping(address=>Patient)public patientRecord;
-    // mapping(address=>bool) public patient_reg;
-    // mapping(address=>string)internal addr_to_str;
     
+    // function which will invoke when patient will give permission to doctor to add his records 
     function giveCreatePermission(address _dkey, address _pkey)public{
         require(msg.sender==_pkey);
         addPermit[_pkey][_dkey]=true;
     }
     
+    // Function to give view permission to doctor for seeing the records of the patient 
     function giveViewPermission(address _dkey, address _pkey)public{
         require(msg.sender==_pkey);
         viewPermit[_pkey][_dkey]=true;
     }
 
+    // function to revoke create permission of a doctor 
     function revokeCreatePermission(address _dkey, address _pkey)public{
         require(msg.sender==_pkey);
         addPermit[_pkey][_dkey]=false;
     }
     
+    // function to revoke view permission of a doctor    
     function revokeViewPermission(address _dkey, address _pkey)public{
         require(msg.sender==_pkey);
         viewPermit[_pkey][_dkey]=false;
     }
 
+    // function to automatically revoke create permission of doctor after adding the record of the patient 
     function autoRevokeCreatePermission(address _dkey, address _pkey)public{
         addPermit[_pkey][_dkey]=false;
     }
     
+    // function to add the patient records
     function addPatientRecord( address _dkey, address _pkey, string memory _dName, string memory _pName,string memory _pGender, uint _pAge, string memory _dov, string memory _pov, string memory _uk) public{
         
         require(msg.sender==_dkey);
@@ -60,7 +63,7 @@ contract medRec{
         records[_uk]=Patient(_dkey, _pkey ,_dName, _pName ,_pGender, _pAge, _dov,  _pov, _uk);
     }
 
-
+    // function to view patient records for doctor
     function viewPatientRecords(address _dkey, address _pkey) public view returns(bool){
         
         if(msg.sender!=_dkey){
@@ -72,6 +75,7 @@ contract medRec{
         return true;
     }
 
+    // function by which patient can view his own records (no permission required)
     function viewOwnRecords(address _pkey) public view returns(bool){
         
         if(msg.sender!=_pkey){
@@ -80,50 +84,5 @@ contract medRec{
         return true;
     }
 
-    // function addPatientRecord(address _dkey, address _pkey, string memory doctorName, string memory patientName,string memory gender, uint age, string memory dov, string memory pv) public{
-        
-    //     require(msg.sender==_dkey);
-    //     require(addPermit[_pkey][_dkey]==true);
-    //     noOfRecords[_pkey]=1;
-    //     // uint id= noOfRecords[_pkey];
-    //     patientRecord[_pkey]= Patient(_dkey, _pkey ,doctorName, patientName ,gender, age, dov,  pv);
-        
-    // }
 
-
-
-    // function viewPatientRecords(address _dkey, address _pkey) public view returns(Patient[] memory){
-        
-    //     require(msg.sender==_dkey);
-    //     require(viewPermit[_pkey][_dkey]==true);
-    //     Patient[]memory patientRecordList=new Patient[](noOfRecords[_pkey]);
-    //     for(uint i=1; i<=noOfRecords[_pkey]; i++) {
-    //         patientRecordList[i-1]=patientRecord[_pkey][i];
-    //     }
-    //     return patientRecordList;
-    // }
-    
-
-    // function viewOwnRecords(address _pkey) public view returns(Patient[] memory){
-        
-    //     require(msg.sender==_pkey);
-    //     Patient[]memory ownRecordList=new Patient[](noOfRecords[_pkey]);
-    //     for(uint i=1; i<=noOfRecords[_pkey]; i++) {
-    //         ownRecordList[i-1]=patientRecord[_pkey][i];
-    //     }
-    //     return ownRecordList;
-    // }   
-
-     // function encodeRecord(address _pkey, uint _id) public returns(string memory){
-    //     bytes memory a1 = abi.encodePacked(_pkey);
-    //     bytes memory a2 = abi.encodePacked("$");
-    //     bytes memory a = abi.encodePacked(a1, a2);
-
-    //     bytes memory b = abi.encodePacked(_id);
-    //     bytes memory c = abi.encodePacked(a, b);
-
-    //     string memory str = string(c);
-
-    //     return str;
-    // }
 }
